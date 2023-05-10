4ce6fd862b2b7414f18ec73e20b08c2b4e1717f9
Enkhe Garmaev <garmaevenhe@gmail.com>
2023-05-10 18:44:35 +0800

    fix: delete Sync

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,78 +1,42 @@
 const fs = require("fs");
 const path = require("path");
 
-const sourceDir = path.join(__dirname, "files");
-const targetDir = path.join(__dirname, "files-copy");
+const copyFiles = async () => {
+  const filesDir = path.join(__dirname, "files");
+  const filesCopyDir = path.join(__dirname, "files-copy");
 
-const copyFile = (file, srcDir, destDir) => {
-  const sourcePath = path.join(srcDir, file);
-  const destPath = path.join(destDir, file);
-  const readStream = fs.createReadStream(sourcePath);
-  const writeStream = fs.createWriteStream(destPath);
-  readStream.pipe(writeStream);
+  try {
+    await fs.promises.access(filesCopyDir);
+  } catch (err) {
+    await fs.promises.mkdir(filesCopyDir);
+  }
+
+  const files = await fs.promises.readdir(filesDir);
+  const filesCopy = await fs.promises.readdir(filesCopyDir);
+
+  // Копирование новых и обновление существующих файлов
+  for (const file of files) {
+    const source = path.join(filesDir, file);
+    const destination = path.join(filesCopyDir, file);
+
+    if (filesCopy.includes(file)) {
+      await fs.promises.unlink(destination);
+    }
+
+    const readStream = fs.createReadStream(source);
+    const writeStream = fs.createWriteStream(destination);
+    readStream.pipe(writeStream);
+  }
+
+  // Удаление лишних файлов
+  for (const fileCopy of filesCopy) {
+    if (!files.includes(fileCopy)) {
+      const fileCopyPath = path.join(filesCopyDir, fileCopy);
+      await fs.promises.unlink(fileCopyPath);
+    }
+  }
+
+  console.log("Копирование файлов успешно завершено");
 };
 
-const deleteFile = (filePath) => {
-  fs.unlink(filePath, (err) => {
-    if (err) throw err;
-    console.log(`File ${filePath} deleted`);
-  });
-};
-
-const copyDirectory = (srcDir, destDir) => {
-  fs.readdir(srcDir, (err, files) => {
-    if (err) throw err;
-
-    files.forEach((file) => {
-      const sourcePath = path.join(srcDir, file);
-
-      fs.stat(sourcePath, (err, stats) => {
-        if (err) throw err;
-
-        if (stats.isDirectory()) {
-          fs.mkdir(path.join(destDir, file), { recursive: true }, (err) => {
-            if (err) throw err;
-            copyDirectory(path.join(srcDir, file), path.join(destDir, file));
-          });
-        } else {
-          copyFile(file, srcDir, destDir);
-        }
-      });
-    });
-  });
-};
-
-// Создание папки files-copy
-fs.mkdir(targetDir, { recursive: true }, (err) => {
-  if (err) throw err;
-
-  // Чтение содержимого папки files и копирование файлов в папку files-copy
-  fs.readdir(sourceDir, (err, files) => {
-    if (err) throw err;
-
-    const filesToCopy = files.filter((file) => {
-      const sourcePath = path.join(sourceDir, file);
-      const stats = fs.statSync(sourcePath);
-      return stats.isFile();
-    });
-
-    // Копирование файлов
-    filesToCopy.forEach((file) => {
-      copyFile(file, sourceDir, targetDir);
-    });
-
-    // Удаление лишних файлов
-    fs.readdir(targetDir, (err, files) => {
-      if (err) throw err;
-
-      const filesToDelete = files.filter((file) => !filesToCopy.includes(file));
-
-      filesToDelete.forEach((file) => {
-        const filePath = path.join(targetDir, file);
-        deleteFile(filePath);
-      });
-    });
-  });
-});
-
-console.log("Копирование завершено");
+copyFiles();
